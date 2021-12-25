b46b5cf5f0232d569a58780f0a073640ca6f92b3
kimura wataru <kimuraw@i.nifty.jp>
2021-12-25 21:23:16 +0900

    Fix error at destroot (`make install DESTDIR=path/to`)
    From MacPorts patch-_bundle_gems-extconf.rb.diff; ruby31 @3.1.0_0 (@560dafc5)
    
    See:
    https://bugs.ruby-lang.org/issues/18373

--- .bundle/gems/debug-1.4.0/ext/debug/extconf.rb
+++ .bundle/gems/debug-1.4.0/ext/debug/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
 require_relative '../../lib/debug/version'
 File.write("debug_version.h", "#define RUBY_DEBUG_VERSION \"#{DEBUGGER__::VERSION}\"\n")
+$arch_hdrdir = "$(hdrdir)/../.ext/include/$(arch)"
+$DLDFLAGS << " -L#{$top_srcdir}"
 create_makefile 'debug/debug'
--- .bundle/gems/rbs-2.0.0/ext/rbs_extension/extconf.rb
+++ .bundle/gems/rbs-2.0.0/ext/rbs_extension/extconf.rb
@@ -1,3 +1,5 @@
 require 'mkmf'
 $INCFLAGS << " -I$(top_srcdir)" if $extmk
+$arch_hdrdir = "$(hdrdir)/../.ext/include/$(arch)"
+$DLDFLAGS << " -L#{$top_srcdir}"
 create_makefile 'rbs_extension'
